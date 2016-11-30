b60d932167c70322ee0782263d7fd1b4a43071b2
ErajSattori <eraj.sattori@gmail.com>
2016-11-30 23:48:37 +0100

    small fixes

--- archives-js/js/var-number.js
+++ archives-js/js/var-number.js
@@ -6,5 +6,5 @@ price = 5;
 quantity = 14;
 total = price * quantity;
 
-var text = document.getElementById('cost');
-text.textContent = '$' + total;
+var el = document.getElementById('cost');
+el.textContent = '$' + total;
