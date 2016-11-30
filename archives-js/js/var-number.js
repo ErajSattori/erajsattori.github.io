0139aabe34bc26b970ce44f6ff1d0ded87a1d3bc
ErajSattori <eraj.sattori@gmail.com>
2016-12-01 00:22:40 +0100

    string added

--- archives-js/js/var-number.js
+++ archives-js/js/var-number.js
@@ -2,9 +2,18 @@ var price;
 var quantity;
 var total;
 
+var username;
+var message;
+username = 'Jack';
+message = 'See our upcoming range';
+
+var elUsername = document.getElementById('name');
+elUsername.textContent = username;
+var elMessage = document.getElementById('message');
+elMessage.textContent = message;
+
 price = 5;
 quantity = 14;
 total = price * quantity;
-
 var el = document.getElementById('cost');
 el.textContent = '$' + total;
