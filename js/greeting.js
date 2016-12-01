bf51827c2e4972183f1ee694f633fe0d93b25c37
ErajSattori <eraj.sattori@gmail.com>
2016-12-01 01:31:31 +0100

    timing adjustment

--- js/greeting.js
+++ js/greeting.js
@@ -9,14 +9,14 @@ var hourNow = today.getHours();
 var greeting;
 
 
-if (hourNow > 17) {
+if (hourNow > 16) {
   greeting = 'Good Evening!';
-} else if (hourNow > 12) {
+} else if (hourNow > 11) {
   greeting = 'Good Afternoon!';
-} else if (hourNow > 0) {
+} else if (hourNow > 5) {
   greeting = 'Good Morning!';
 } else {
-  greeting = 'Hey!';
+  greeting = 'Good Evening!';
 }
 
 document.write(greeting + ' I am...<br />');
